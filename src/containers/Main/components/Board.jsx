899371ddf08271a566e3549c7e34c4d84af94319
mojisilva <mojiasilva@gmail.com>
2022-04-13 16:43:22 -0300

    updating project

---
+++ src/containers/Main/components/Board.jsx
@@ -0,0 +1,40 @@
+import React, {memo} from "react";
+import PropTypes from 'prop-types';
+import Grid from "../../../components/Grid";
+import Skeleton from "../../../components/Skeleton";
+import Card from "./Card";
+
+function Board({data}){
+    const {cases, todayDeaths, recovered, deaths, todayCases} = data
+    
+    const getValue = (value) => value ? value : <Skeleton variant='text' width={182} height={60}></Skeleton>
+
+    return(
+        <Grid container spacing = {4}>
+            <Grid item xs={12} md={3}>
+                <Card value={getValue(cases)} label="Total de casos" color="#5D78ff">
+                </Card>
+            </Grid>
+            <Grid item xs={12} md={3}>
+                <Card value={getValue(todayDeaths)} label="Óbitos Hoje" color="#F7B829">
+                </Card>
+            </Grid>
+
+            <Grid item xs={12} md={3}>
+                <Card value={getValue(todayCases)} label="Casos Hoje" color="#000000">
+                </Card>
+            </Grid>
+
+            <Grid item xs={12} md={3}>
+                <Card value={getValue(deaths)} label="Total de Mortos" color="#E95078">
+                </Card>
+            </Grid>
+            <Grid item xs={12} md={3}>
+                <Card value={getValue(recovered)} label="Total de Recuperados" color="#67C887">
+                </Card>
+            </Grid>
+
+        </Grid>
+    )
+}
+export default memo(Board)
