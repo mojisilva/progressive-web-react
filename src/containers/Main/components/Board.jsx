c43bb489a00e5c105122d5782da759790848839b
mojisilva <mojiasilva@gmail.com>
2022-06-09 21:06:06 -0300

    updating project

--- src/containers/Main/components/Board.jsx
+++ src/containers/Main/components/Board.jsx
@@ -12,25 +12,27 @@ function Board({data}){
         <>
             <Grid container spacing={3}>
                 <Grid item xs={12} md={4}>
-                    <Card value={getValue(todayCases).toLocaleString('pt-BR')} label="Casos Hoje" color="#000000">
+                    <Card value={getValue(todayCases.toLocaleString('pt-BR'))} label="Casos Hoje" color="#000000">
                     </Card>
                 </Grid>
                 <Grid item xs={12} md={4}>
-                    <Card value={getValue(todayDeaths).toLocaleString('pt-BR')} label="Óbitos Hoje" color="#F7B829">
+                    <Card value={
+                        
+                        getValue(todayDeaths.toLocaleString('pt-BR'))} label="Óbitos Hoje" color="#F7B829">
                     </Card>
                 </Grid>
             </Grid>
             <Grid container spacing={3}>
                 <Grid item xs={12} md={4}>
-                    <Card value={getValue(cases).toLocaleString('pt-BR')} label="Total de casos" color="#5D78ff">
+                    <Card value={getValue(cases.toLocaleString('pt-BR'))} label="Total de casos" color="#5D78ff">
                     </Card>
                 </Grid>
                 <Grid item xs={12} md={4}>
-                    <Card value={getValue(deaths).toLocaleString('pt-BR')} label="Total de Óbitos" color="#E95078">
+                    <Card value={getValue(deaths.toLocaleString('pt-BR'))} label="Total de Óbitos" color="#E95078">
                     </Card>
                 </Grid>
                 <Grid item xs={12} md={4}>
-                    <Card value={getValue(recovered).toLocaleString('pt-BR')} label="Total de Recuperados" color="#67C887">
+                    <Card value={getValue(recovered.toLocaleString('pt-BR'))} label="Total de Recuperados" color="#67C887">
                     </Card>
                 </Grid>
             </Grid>
